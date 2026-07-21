71bd0a3c500733b56a6a9856973af17b5d63ed12
CristopheGomes <cristophegomes33@gmail.com>
2026-07-21 15:38:07 -0300

    atualizando

--- Jogodavelha/Program.cs
+++ Jogodavelha/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System; 
 class Program
 {
+    
     static void Main(string{} args)
     {
         Console.Title = "Jogo da Velha";
 
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("=======================================")
-        Console.WriteLine("             Jogo da Velha")
-        Console.WriteLine("=======================================")
+        Console.WriteLine("=======================================");
+        Console.WriteLine("             Jogo da Velha");
+        Console.WriteLine("=======================================");
         Console.ResetColor();
     }
 }
