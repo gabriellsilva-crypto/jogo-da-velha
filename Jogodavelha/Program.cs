d28ccdb2df5fa61aa381b8b70fda98b5023c2419
Galaland67 <madirce.graca@aluno.senai.br>
2026-07-21 15:33:09 -0300

    Exibição do tabuleiro.

--- Jogodavelha/Program.cs
+++ Jogodavelha/Program.cs
@@ -1,1 +1,27 @@
 ﻿                            //primeira etapa
+using System;
+class Program 
+  {
+    static void Main(string[] args)
+     {
+        Console.Title = "Jogo da Velha";
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("=======================");
+        Console.WriteLine("         JOGO DA VELHA");
+        Console.WriteLine("=======================");
+        Console.ResetColor();
+
+          Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para iniciar...");
+        Console.ReadLine();
+
+
+        Console.Clear();
+
+        char[,] tabuleiro =
+        {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
