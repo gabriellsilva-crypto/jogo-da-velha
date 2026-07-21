a1bdbe27142d0e6405577009d81631df885619e0
Galaland67 <madirce.graca@aluno.senai.br>
2026-07-21 15:46:34 -0300

    Códgio atualizado

--- Jogodavelha/Program.cs
+++ Jogodavelha/Program.cs
@@ -22,22 +22,9 @@ class Program
         char[,] tabuleiro =
         {
             { '1', '2', '3' },
-            { '4', '5', '6' },
+            { '4', '', '6' },
             { '7', '8', '9' }
         };
      MostrarTabuleiro(tabuleiro);
-    }
-
-    static void MostrarTabuleiro(char[,] tabuleiro)
-    {
-        Console.WriteLine();
-
-        Console.WriteLine($" {tabuleiro[0, 0]} | {tabuleiro[0, 1]} | {tabuleiro[0, 2]} ");
-        Console.WriteLine("---+---+---");
-        Console.WriteLine($" {tabuleiro[1, 0]} | {tabuleiro[1, 1]} | {tabuleiro[1, 2]} ");
-        Console.WriteLine("---+---+---");
-        Console.WriteLine($" {tabuleiro[2, 0]} | {tabuleiro[2, 1]} | {tabuleiro[2, 2]} ");
-
-        Console.WriteLine();
     }
 }
